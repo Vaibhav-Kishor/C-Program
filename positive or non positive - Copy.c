57244b1de9bc1952d332f244d7ff8b023525d756
Vaibhavkishor <vaibhavkishor7@gmail.com>
2019-07-28 03:58:19 -0700

    some program deleted
    Some program deleted Cause of dublication.

--- positive or non positive - Copy.c
+++ positive or non positive - Copy.c
@@ -1,20 +1,4 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-  int x;
-  clrscr();
-  printf("\n checking which is positive and non positive");
-  printf("ENTER YOUR NUMBER =>");
   scanf("%d",&x);
-  if(x>0)
-  {
-	  printf("\n your Entered number is positive");
 	 
-  }	
   if(x<=0)
-  {
-	  printf("\n your Entered number is non-positive");
   }
-  getch();
-}
